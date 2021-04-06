04410d1d9e176b3dca4d8390e7185480876991fa
justnyc <jcairo187@gmail.com>
2021-04-06 11:30:32 -0400

    begginings of opening window

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,10 @@
-// M.V.P
+                                                // M.V.P
 
-// Object - keep beer "frosty"
+// MISSION - "keep beer frosty"
 
 // User Story
                     // UI 1
+// Opening UI will have a box with a name input 
 // User types in name in the opening "name" window and pushes the enter button and game begins
 
 
@@ -19,9 +20,11 @@
 
 
                     // UI 2
+// User will see a page with Users "name" displayed, a "frosty" beer mug, a thermostat and a button that says "frost"(under mug)
 
 // User will have to click "frost" button in order to keep thermostat below maximum allowed temp, in the given time frame of the game
-// User will see a page with their "name" displayed, a "frosty" beer mug, a thermostat and a button that says "frost"(under mug)
+
+
 
 
 /* more details */
@@ -32,6 +35,7 @@
 
 //  - User will have to keep the mug "frosty"(temp low), by clicking the "frost" button, for the given amount of time, in order to win the game
 
+/* more details */
 // click "frost" button to increase temp by 10 - event listener
 // temp is < 50 && game timer = 0 User wins
 // if temp is > 50 && game timer = 0 User loses
