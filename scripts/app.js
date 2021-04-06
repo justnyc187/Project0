00b3e6531cb53138645b003b23438c7004d7ebb1
justnyc <jcairo187@gmail.com>
2021-04-06 00:11:52 -0400

    MVP

---
+++ scripts/app.js
@@ -0,0 +1,45 @@
+// M.V.P
+
+// Object - keep beer "frosty"
+
+// User Story
+                    // UI 1
+// User types in name in the opening "name" window and pushes the enter button and game begins
+
+
+/* more details */ 
+// When User types in name and hit enter button it will trigger event listeners
+/* enter button event listeners */
+// name will be appended to the header of the 2nd UI
+// game time will begin
+// thermostat timer will also begin
+// 2nd UI will open
+
+
+
+
+                    // UI 2
+
+// User will have to click "frost" button in order to keep thermostat below maximum allowed temp, in the given time frame of the game
+// User will see a page with their "name" displayed, a "frosty" beer mug, a thermostat and a button that says "frost"(under mug)
+
+
+/* more details */
+// 2nd UI page will open 
+// therostat timer will begin at a default of 0 -> event listener
+// thermostat will increase by 10 ever second -> loop
+// game timer will begin and count down from 30 to 0
+
+//  - User will have to keep the mug "frosty"(temp low), by clicking the "frost" button, for the given amount of time, in order to win the game
+
+// click "frost" button to increase temp by 10 - event listener
+// temp is < 50 && game timer = 0 User wins
+// if temp is > 50 && game timer = 0 User loses
+
+
+
+
+
+
+
+
