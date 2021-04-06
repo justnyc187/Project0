7449d595f7472df224138e115642b4a317e87ba0
justnyc <jcairo187@gmail.com>
2021-04-06 17:09:10 -0400

    User story and window

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,51 @@
-                                                // M.V.P
+console.log("Hi Justin");                    
+                    
+                    // M.V.P
 
-// MISSION - "keep beer frosty"
+// MISSION - "keep tomagotchi alive"
 
 // User Story
                     // UI 1
 // Opening UI will have a box with a name input 
-// User types in name in the opening "name" window and pushes the enter button and game begins
+// User types in name in the opening "name" window and pushes the click button and game begins
 
 
 /* more details */ 
-// When User types in name and hit enter button it will trigger event listeners
-/* enter button event listeners */
-// name will be appended to the header of the 2nd UI
-// game time will begin
-// thermostat timer will also begin
-// 2nd UI will open
+// When User types in name and hits click button it will trigger a multitude of event listeners
 
 
+/* click button event listeners on game begin */
+// name will be appended to the second section of the UI 
+// Hunger timer/bar will increase ++1 every 5000ms - if hunger reaches 10 tomagatchi will die
+// Boredom timer/bar will increase ++1 every 4000ms - if boredom reaches 10 tomagatchi will die
+// Sleepiness timer/bar with increase ++1 every 7000ms - if sleepiness reaches 10 tomagatchi will die
+// Age timer will start at a default of 120,000ms and decrease by 1000ms - if tomagatchi is still alive after 120,000 - baby tomagatchi will spawn into nigger tomagatchi
 
-
-                    // UI 2
-// User will see a page with Users "name" displayed, a "frosty" beer mug, a thermostat and a button that says "frost"(under mug)
-
-// User will have to click "frost" button in order to keep thermostat below maximum allowed temp, in the given time frame of the game
-
-
-
+                    // UI Section 2
+// User will see a page with Users "name" displayed
+// the tomagatchi
+// Sleepiness bar 
+// Boredom bar
+// Hunger bar
+// tomagatchi age
+// User will have to click buttons in order to keep tomagatchi from being Hungry, Tired or Bored
 
 /* more details */
 // 2nd UI page will open 
-// therostat timer will begin at a default of 0 -> event listener
-// thermostat will increase by 10 ever second -> loop
-// game timer will begin and count down from 30 to 0
+// tomagatchi age will increase ++1 if Boredom, Hunger and Sleepiness are < 10 && age timer = 0
+// Hunger timer will begin at a default of 0 -> event listener - and it will ++1 every 5000ms
+// Boredom timer will begin at a default of 0 -> event listener - and it will increase ++1 every 4000ms
+// Sleepiness timer will begin at a default of 0 -> event listener - and it will ++1 every 7000ms
+// Age timer will begin at 120,000ms and decrease by 1000ms
+// tomagotchi will walk around while it is alive
+// if Tomagatchi's Sleepiness, Boredom or Hunger reaches 10, tomagatchi will die
+// if Boredom, Sleepiness and Hunger timer < 10 && Age time is = 0, tomagatchi will spawm + age will increase ++1
+// text will appear "'User' is now 1 year old" and baby 
+//
 
-//  - User will have to keep the mug "frosty"(temp low), by clicking the "frost" button, for the given amount of time, in order to win the game
+
+
+//  - User will have to 
 
 /* more details */
 // click "frost" button to increase temp by 10 - event listener
@@ -44,6 +56,11 @@
 
 
 
+class babyTomagatchi = {
+    name: "",
+    age: 0,
+    boredom: 0
 
+}
 
 
